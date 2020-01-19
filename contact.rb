77a204f6b525ffeffb7113254a941adfa8aa9e39
Ari Drookman <aga.drookman@gmail.com>
2020-01-19 05:03:50 -0500

    Yossi's turn

--- contact.rb
+++ contact.rb
@@ -1,8 +1,12 @@
 class Contact
 
   # This method should initialize the contact's attributes
-  def initialize
-
+  
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
   end
 
   # This method should call the initializer, 
